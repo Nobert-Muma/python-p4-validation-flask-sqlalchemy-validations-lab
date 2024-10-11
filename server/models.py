5197c12567ac1dc76f6cef762cd273590908b249
Nobert-Muma <mumanobert17@gmail.com>
2024-10-12 00:44:43 +0300

    Flask-SQLAlchemy Validations Lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,23 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        author=Author.query.filter_by(name=name).first()
+        if len(name) == 0:
+            raise ValueError("All authors must have a name.")
+        if author is not None:
+            raise ValueError("No two authors can have the same name.")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and len(phone_number) != 10:
+            raise ValueError("Author phone numbers must be exactly ten digits.")
+        if phone_number and not phone_number.isdigit():
+            raise ValueError("Author phone numbers must be numeric.")
+        return phone_number
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +44,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Post title is sufficiently clickbait-y and must contain one of the following: (Won't Believe, Secret, Top, Guess)")
+        return title
 
+    @validates('content') 
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError("Post content is at least 250 characters long.")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Post summary is a maximum of 250 characters.")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category!='Fiction' and category!='Non-Fiction':
+            raise ValueError('Post category is either Fiction or Non-Fiction.')
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
